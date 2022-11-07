4321d27160e62a9d292f3f21bc4d68023d157977
Nada-Elfazary <elfazary@princeton.edu>
2022-11-06 22:11:03 -0500

    Added store functions

--- processing.py
+++ processing.py
@@ -13,6 +13,8 @@ DATABASE_URL = 'postgres://hwwlwcbv:hyNZQS9_LH8CSD3yQoc5IpDHkBJeSlhF@peanut.db.e
 
 DICT_DAY = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
 
+EVENT_ID = 10 #??
+
 def get_day():
     return datetime.datetime.now().weekday()
 
@@ -71,11 +73,86 @@ def fetch_activities():
         print(ex, file=sys.stderr)
         sys.exit(1)
 
+def store_activity(activity):
+    title = activity[0]
+    location = activity[1]
+    date = activity[2]
+    start_time = activity[3]
+    end_time = activity[4]
+    cap = activity[5]
+    cost = activity[6]
+    description = activity[7]
+    category = activity[8]
+
+    #******
+    creator = 'elfazary'
+
+    try:
+        database_url = DATABASE_URL
+        event_id = EVENT_ID+1
+        with psycopg2.connect(database_url) as connection:
+            
+            with connection.cursor() as cursor:
+                day = get_day()
+                statement = "INSERT INTO events VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                cursor.execute(statement, (event_id, title, start_time,
+                end_time, cap, creator, category, location, description,
+                cost, date, 0))            
+
+    except Exception as ex:
+        print(ex, file=sys.stderr)
+        sys.exit(1)
+
+def store_sign_up(sign_up):
+    netid = 'elfazary' #hardcoded for now
+    eventid = sign_up[0]
+    name = sign_up[1]
+    phone_num = sign_up[2]
+    email = sign_up[3]
+
+
+    try:
+        database_url = DATABASE_URL
+        with psycopg2.connect(database_url) as connection:
+            
+            with connection.cursor() as cursor:
+                day = get_day()
+               # statement = "SELECT "
+
+               # UPDATE EVENTS TABLE
+                statement = "UPDATE events SET signedup_number"
+                + "= %s WHERE eventid = %s"
+
+                cursor.execute(statement, ('signedup_number + 1', eventid))
+
+                # UPDATE SIGNUP TABLE
+                statement = "INSERT INTO signup VALUES"
+                + "(%s, %s, %s)"  
+                cursor.execute(statement, (eventid, netid, 0))       
+
+    except Exception as ex:
+        print(ex, file=sys.stderr)
+        sys.exit(1)
+
+
+
+
+
+
 def main():
 
     if len(sys.argv) != 1:
         print('Usage: python display.py', file=sys.stderr)
         sys.exit(1)
+
+    start_time = datetime.time(12, 30)
+    end_time = datetime.time(13, 30)
+    date = datetime.date(2022, 11, 8)
+
+    store_activity(['Canoeing', 'Carnegie Lake', date, start_time, end_time, 20,
+    100, 'Canoeing at carnegie lake', 'Off-campus'])
+
+    fetch_activities()
 
     
 
