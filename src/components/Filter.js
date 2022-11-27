263ba3f72b835c3abb8a61451531b55927c8852f
Anca Negoiu <anegoiu@princeton.edu>
2022-11-27 09:37:13 -0500

    add cancel buttons to dropdown filters

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import Button from 'react-bootstrap/Button';
 import "./Filter.css";
 export default function Filter(props) : React.ReactNode {
     const [title, setTitle] = useState("")
@@ -50,41 +51,58 @@ export default function Filter(props) : React.ReactNode {
                 </div>
                 <div className="dayFilter">
                     <label>Day</label>
-                    <DropdownButton 
-                        title= {day == "" || null ? "Day" : day}
-                        variant="warning"
-                        onSelect={(key, event) => 
-                            handleSelect(key, event, "dayFilter")}
-                    >
-                        <Dropdown.Item>Monday</Dropdown.Item>
-                        <Dropdown.Item>Tuesday</Dropdown.Item>
-                        <Dropdown.Item>Wednesday</Dropdown.Item>
-                        <Dropdown.Item>Thursday</Dropdown.Item>
-                        <Dropdown.Item>Friday</Dropdown.Item>
-                        <Dropdown.Item>Saturday</Dropdown.Item>
-                        <Dropdown.Item>Sunday</Dropdown.Item>
-                    </DropdownButton> 
+                    <div className="alignFilter">
+                        <DropdownButton 
+                            title= {day == "" || null ? "Day" : day}
+                            variant="warning"
+                            onSelect={(key, event) => 
+                                handleSelect(key, event, "dayFilter")}
+                        >
+                            <Dropdown.Item>Monday</Dropdown.Item>
+                            <Dropdown.Item>Tuesday</Dropdown.Item>
+                            <Dropdown.Item>Wednesday</Dropdown.Item>
+                            <Dropdown.Item>Thursday</Dropdown.Item>
+                            <Dropdown.Item>Friday</Dropdown.Item>
+                            <Dropdown.Item>Saturday</Dropdown.Item>
+                            <Dropdown.Item>Sunday</Dropdown.Item>
+                        </DropdownButton>
+                        <button 
+                        className= "FilterX" 
+                        onClick={() => {
+                            setDay("")
+                            props.getEvents(false, title, "", category)
+                            }}>
+                            X
+                        </button>
+                    </div>
                 </div>
 
                 <div className="categoryFilter">
-                        <label>Category</label>
-                    <DropdownButton 
-                        title= {category == "" || null ? "Category" : category} 
-                        variant="warning"
-                        onSelect={(key,event) => 
-                            handleSelect(key, event, "categoryFilter")}
-                    >
-                        <Dropdown.Item>Sports</Dropdown.Item>
-                        <Dropdown.Item>Entertainment</Dropdown.Item>
-                        <Dropdown.Item>Academic</Dropdown.Item>
-                        <Dropdown.Item>Off-campus</Dropdown.Item>
-                        <Dropdown.Item>Outdoors</Dropdown.Item>
-                        <Dropdown.Item>Meals/Cofee Chats</Dropdown.Item>
-                        <Dropdown.Item>Nassau Street</Dropdown.Item>
-                        {/* Meals/Coffee Chats", "Nassau Street" */}
-                    </DropdownButton>
-
-
+                    <label>Category</label>
+                    <div className="alignFilter">
+                        <DropdownButton 
+                            title= {category == "" || null ? "Category" : category} 
+                            variant="warning"
+                            onSelect={(key,event) => 
+                                handleSelect(key, event, "categoryFilter")}
+                        >
+                            <Dropdown.Item>Sports</Dropdown.Item>
+                            <Dropdown.Item>Entertainment</Dropdown.Item>
+                            <Dropdown.Item>Academic</Dropdown.Item>
+                            <Dropdown.Item>Off-campus</Dropdown.Item>
+                            <Dropdown.Item>Outdoors</Dropdown.Item>
+                            <Dropdown.Item>Meals/Cofee Chats</Dropdown.Item>
+                            <Dropdown.Item>Nassau Street</Dropdown.Item>
+                        </DropdownButton>
+                        <button 
+                            className= "FilterX" 
+                            onClick={() => {
+                                setCategory("")
+                                props.getEvents(false, title, day, "")
+                                }}>
+                                X
+                        </button> 
+                    </div>
                 </div>
             </div>
         </div>
