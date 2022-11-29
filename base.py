e366495695e56dc88143661a1b6d0614344ed134
Nada-Elfazary <elfazary@princeton.edu>
2022-11-29 15:18:42 -0500

    Going back to the less strict cors policy

--- base.py
+++ base.py
@@ -10,8 +10,8 @@ from flask_cors import CORS
 app = flask.Flask(__name__)
 #app.config['CORS_HEADERS'] = 'Content-Type'
 app.secret_key = os.environ['APP_SECRET_KEY']
-#CORS(app)
-cors = CORS(app, resources={r"/*": {"origins": "https://tigeractivities-iqwe.onrender.com"}})
+CORS(app)
+#cors = CORS(app, resources={r"/*": {"origins": "https://tigeractivities-iqwe.onrender.com"}})
 
 @app.route('/logoutapp', methods=['GET'])
 @app.route('/logoutcas', methods=['GET'])
