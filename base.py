a3e32edd3f4d9314656102a4755ea6fd8508625f
Reuben Agogoe <ragogoe@princeton.edu>
2022-11-30 19:32:44 -0500

    idk what the problem is anymore

--- base.py
+++ base.py
@@ -30,7 +30,7 @@ def nishan():
 
 @app.after_request
 def after_request(response):
- response.headers.set('Access-Control-Allow-Origin')
+ response.headers.add('Access-Control-Allow-Origin', 'http://tigeractivities-iqwe.onrender.com')
   #response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
  # response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
  # return response
