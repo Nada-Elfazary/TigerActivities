e5fe8e28b1c8c503e0199fae2f4d69e7b324ee2b
Reuben Agogoe <ragogoe@princeton.edu>
2022-11-30 19:54:12 -0500

    testing with set instead of add

--- base.py
+++ base.py
@@ -30,7 +30,7 @@ def nishan():
 
 @app.after_request
 def after_request(response):
- response.headers.add('Access-Control-Allow-Origin', 'https://tigeractivities-iqwe.onrender.com')
+ response.headers.set('Access-Control-Allow-Origin', 'https://tigeractivities-iqwe.onrender.com')
   #response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
  # response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
  # return response
