99284c79948d5b9600c4894822af936ae4daa4ba
Reuben Agogoe <ragogoe@princeton.edu>
2022-12-01 23:58:06 -0500

    trying login

--- src/components/CasPage.js
+++ src/components/CasPage.js
@@ -1,5 +1,5 @@
 import {React, useEffect} from 'react'
-export default function CasPage(){
+export default function CasPage(props){
     
         useEffect(() => {
           window.location.replace(props.url);
@@ -7,7 +7,7 @@ export default function CasPage(){
       
         // Render some text when redirecting
         // You can use a loading gif or something like that
-        return <div style={styles.container}>
+        return <div>
           <h3>Redirecting...</h3>
         </div>
       
