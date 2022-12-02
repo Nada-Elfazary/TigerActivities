5d535563ade32d1fb8b8880d59d75a0b1ede606e
Anca Negoiu <anegoiu@princeton.edu>
2022-12-01 21:18:49 -0500

    try to add cross origin header on axios

--- src/components/RulesModal.js
+++ src/components/RulesModal.js
@@ -15,7 +15,12 @@ export default function RulesModal(props) : React.ReactNode {
   
 
     const cas = ()=>{
-      axios.get('https://tigeractivities.onrender.com/dummy').then(res=>{
+      const config = {
+        headers: {
+          "Access-Control-Allow-Origin" : "true"
+        }
+      }
+      axios.get('https://tigeractivities.onrender.com/dummy', config=config).then(res=>{
       let url = res
       console.log("url", url)
         navigate('/homeTo')
