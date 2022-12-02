416b3438bf58497fb11b067ec8148476af58f565
Reuben Agogoe <ragogoe@princeton.edu>
2022-12-01 23:45:37 -0500

    trying login

--- src/components/RulesModal.js
+++ src/components/RulesModal.js
@@ -20,20 +20,17 @@ export default function RulesModal(props) : React.ReactNode {
       //let url = res
       // console.log("url", url)
         navigate('/login')
-      // const config = {
-      //   headers: {
-      //     "Access-Control-Allow-Origin" : "true"
-      //   }
-      // }
+      /* const config = {
+        headers: {
+         "Access-Control-Allow-Origin" : "true"
+        }
+      /* }
       // axios.get('https://tigeractivities.onrender.com/dummy').then(res=>{
       //let url = res
       //console.log("url", url)
         //navigate('/homeTo')
       
-    }.catch(err=>{
-      console.log(err)
-    })
-  }
+    }
 
 
     const failureCallBack = (error)=>{
