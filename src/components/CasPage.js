7cbd1a06fa6b2d483e92062812c28c85131e5ad1
Reuben Agogoe <ragogoe@princeton.edu>
2022-12-02 00:21:31 -0500

    trying login

--- src/components/CasPage.js
+++ src/components/CasPage.js
@@ -1,8 +1,12 @@
 import {React, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+import Home from './Home'
 export default function CasPage(props){
-    
+    const navigate = useNavigate()
+
         useEffect(() => {
-         window.location.replace('https://fed.princeton.edu/cas/login?service=https%3A//tigeractivities-iqwe.onrender.com/');
+         window.location.replace('https://fed.princeton.edu/cas/login?service=https%3A//tigeractivities-iqwe.onrender.com/')
+         navigate("/home")
         }, [])
       
         // Render some text when redirecting
