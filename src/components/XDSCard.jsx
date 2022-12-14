74eea034a67e5edaacc20f3841da301c77323dd8
Anca Negoiu <anegoiu@princeton.edu>
2022-12-14 05:00:29 -0500

    fix build error in XDSCard

--- src/components/XDSCard.jsx
+++ src/components/XDSCard.jsx
@@ -8,23 +8,26 @@ import "./Home.css";
 import axios from 'axios';
 import { propTypes } from 'react-bootstrap/esm/Image';
 
-const XDSCard = ({item, ownerView, setEvents, setPaginatedEvents, username, pageSize, setLoading, signUpsView,name, phone, email, tagColor}) => {
+const XDSCard = ({item, setEvents, setPaginatedEvents, setLoading, pageSize, ownerView, signUpsView,name, phone, email, tagColor, username}) => {
     const [isExpanded, setExpanded] = useState(false)
     const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded })
     const [displaySignUp, setDisplaySignUp] = useState(false)
     const [eventTitle, setEventTitle] = useState('')
    // const [events, setEvents] = useState([])
     const [displayModal, setDisplayModal] = useState(false)
-   // const [loading, setLoading] = useState(false)
+   //const [loading, setLoading] = useState(false)
     const [id, setEventId] = useState('')
     const [attendees, setAttendees] = useState([])
     const [displayCancel, setDisplayCancel] = useState(false)
+    const [isCurrUserSignedUp, setIsCurrUserSignedUp] = useState(false)
     const backgroundColor = tagColor
+
+    
    // const [activityData, setActivityData] = useState(["","","","","","","",""])
   //  const [displayEditModal, setDisplayEditModal] = useState(false)
     const closedText = "(CLOSED)"
 
-    console.log(item.event_name, 'color: ', tagColor)
+    // console.log(item.event_name, 'color: ', tagColor)
 
 
     const numToDay = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 
@@ -33,25 +36,24 @@ const XDSCard = ({item, ownerView, setEvents, setPaginatedEvents, username, page
     const numToMonth = {1: "Jan.", 2: "Feb.", 3: "Mar.", 4: "Apr.", 5: "May",  
     6: "June", 7: "July", 8: "Aug.", 9: "Sept.", 10: "Oct.", 11: "Nov.", 12: "Dec."}
 
-   
-    
     
   const handleSignUp = ()=>{
     setDisplaySignUp(true)
     setEventTitle(item.event_name)
     setEventId(item.id)
+    get_attendees(item.id)
   
   }
 
   
-  const handleCancellation = ()=>{
+  const handleCancellation = () => {
     console.log("canceling sign-up")
     console.log(item.id)
     console.log(item.event_name)
-
     setDisplayCancel(true)
     setEventTitle(item.event_name)
     setEventId(item.id)
+    setIsCurrUserSignedUp(false) 
   }
 
   const handleEdit = ()=>{
@@ -59,13 +61,14 @@ const XDSCard = ({item, ownerView, setEvents, setPaginatedEvents, username, page
     console.log(item.id)
     console.log(item.event_name)
     setDisplayModal(true);
-ignUp    console.log("display modal state: ", displayModal)
+    console.log("display modal state: ", displayModal)
   }
   
   const editModal = displayModal ? (<EditEventDialog setOpenModal = {setDisplayModal} setLoading ={setLoading} setEvents ={setEvents} 
-    setPaginatedEvents = {setPaginatedEvents} pageSize = {pageSize} events = {item} username={username}/>) : null 
+    setPaginatedEvents = {setPaginatedEvents} pageSize = {pageSize} events = {item} />) : null 
 
-  const signUpModal = displaySignUp ? (<SignUpModal setOpenSignUpModal={setDisplaySignUp} title ={eventTitle} event_id={id} username={username}
+  const signUpModal = displaySignUp ? (<SignUpModal setOpenSignUpModal={setDisplaySignUp} title ={eventTitle} event_id={id}
+    username={username}
     name={name}
     phone={phone}
     email={email}
@@ -76,15 +79,23 @@ ignUp    console.log("display modal state: ", displayModal)
 
   const get_attendees = (event)=>{
     console.log("inside get attendees")
-    axios.get('https://tigeractivities.onrender.com/attendees', {params: {
+    axios.get('/attendees', {params: {
       event_id : event.id,
     }}).then(res =>{ 
-            setAttendees(res.data)     
+            setAttendees(res.data)
+            console.log("Iterating thorugh attendees:", res.data)
+            for (const attendee of res.data) {
+              console.log("Attendee:", attendee)
+              if (attendee["netid"] === username){
+                setIsCurrUserSignedUp(true)
+              }
+            }     
     }).catch(err =>{
       console.log(err)
     
     })
   }
+  get_attendees(id)
   const closed = item.signup_number === item.maxcap ? (<p>{closedText}</p>) : null
 
   return (
@@ -95,15 +106,15 @@ ignUp    console.log("display modal state: ", displayModal)
         <Card.Subtitle> <h5><a className='tag' style={{'backgroundColor': backgroundColor}}><text className='white'>{item.category}</text></a></h5></Card.Subtitle>
         <Card.Text> 
                 <Row>
-                <h5 className='date'><strong>{numToDay[item.start_week_day]} {numToMonth[item.start_date.split("/")[1]]} {item.start_date.split("/")[2]}, {item.start_time}</strong></h5>
+                <h5 className='date'><strong>{numToDay[item.week_day]} {numToMonth[item.start_date.split("/")[1]]} {item.start_date.split("/")[2]}, {item.start_time}</strong></h5>
                 </Row>
                 <Row>
-          <Col><strong>End time : </strong>{numToDay[item.end_week_day]} {numToMonth[item.end_date.split("/")[1]]} {item.end_time}</Col>
+          <Col><strong>End time : </strong>{item.end_time}</Col>
           <Col><strong>Location : </strong>{item.location}</Col>
             
           </Row>
         </Card.Text>
-          <Row>
+        <Row>
             <Col><p {...getCollapseProps()}><strong>Number of attendees :</strong> {item.signup_number}/{item.maxcap}</p></Col>
             <Col><p {...getCollapseProps()}><strong>Estimated Cost : </strong>$ {item.cost}</p></Col>
           </Row>
@@ -117,14 +128,20 @@ ignUp    console.log("display modal state: ", displayModal)
 
           <Col>
           {(!ownerView && !signUpsView) ? (<p {...getCollapseProps()}>
-                        <Button  
-                        variant="warning"
-                        onClick={handleSignUp} disabled={item.signup_number === item.maxcap}>Sign Up</Button>            
+                        {isCurrUserSignedUp?
+                        <Button variant="success" disabled>Signed Up</Button>
+                        :
+                         <Button  
+                         variant="warning"
+                         onClick={handleSignUp} disabled={item.signup_number === item.maxcap}>Sign Up</Button> 
+                        
+                        }
+                                  
                            </p>) : null }  
          
             </Col>
           </Row>
-            <Row>
+          <Row>
               <Col>
               {ownerView ? (
                             <p {...getCollapseProps()}>
@@ -151,11 +168,11 @@ ignUp    console.log("display modal state: ", displayModal)
                                 </Table>
                             </p>) : null }
               </Col>
-            </Row> 
+            </Row>
           </Card.Text>
           <Card.Text>
             <Row>
-          <Col><Button classname='Button'
+          <Col><Button className='Button'
           variant="warning"
         {...getToggleProps({
           onClick: () =>{ setExpanded((prevExpanded) => !prevExpanded)
