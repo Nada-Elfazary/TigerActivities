84fac70d0d1773c0dea4744262c1c729883af0ad
Nada-Elfazary <elfazary@princeton.edu>
2022-12-15 18:10:21 -0500

    add authentcation check to Error route

--- src/components/Error.js
+++ src/components/Error.js
@@ -7,6 +7,20 @@ import Card from 'react-bootstrap/Card';
 
 // takes in te error message that should be displayed
 export default function Error(props) : React.ReactNode {
+
+    const cas = ()=>{ 
+        console.log("inside cas")
+        fetch('https://tigeractivities.onrender.com/api/authenticate').then((resp)=>
+          {return resp.text();}).then((data) => {
+          let response = JSON.parse(data)
+          console.log('data from cas: ', data)
+          if(response.username === ''){
+            console.log('redirect url')
+            window.location.replace(response.redirect);
+            console.log('username: ', response.username)
+          } 
+        })
+        
         return (
             <Card className="errorBackground">
                 <div className="errMsgContainer">
