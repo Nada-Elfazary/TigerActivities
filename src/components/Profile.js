1ebe2ca87c0a3cdf5a13a10474d0ae77bc5cb312
Nada-Elfazary <elfazary@princeton.edu>
2023-01-15 01:23:25 +0200

    fixing profile bug

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -26,7 +26,7 @@ export default function Profile(props) {
     console.log("Name in Profile:", props.profileData[0])
 
     
-    const editProfileModal = showModal ? (<EditProfileModal setOpenModal = {setShowModal}  userDetails = {props.profileData} getProfileData = {props.getProfileData}/>) : null 
+    const editProfileModal = showModal ? (<EditProfileModal setOpenModal = {setShowModal}  userDetails = {props.profileData} getProfileData = {props.getProfileData} netid = {props.netid}/>) : null 
 
 
     return (
